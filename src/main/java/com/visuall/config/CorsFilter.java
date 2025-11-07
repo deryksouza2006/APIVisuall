78c893c0d2e1a34ceb03e5a1fc2585729fb6235d
deryksouza2006 <deryksouza2006@gmail.com>
2025-11-07 20:45:47 -0300

    cors config

--- src/main/java/com/visuall/config/CorsFilter.java
+++ src/main/java/com/visuall/config/CorsFilter.java
@@ -1,5 +1,3 @@
-
-
 package com.visuall.config;
 
 import jakarta.ws.rs.container.ContainerRequestContext;
@@ -8,18 +6,23 @@ import jakarta.ws.rs.container.ContainerResponseFilter;
 import jakarta.ws.rs.ext.Provider;
 import java.io.IOException;
 
-@Provider 
+@Provider
 public class CorsFilter implements ContainerResponseFilter {
 
     @Override
     public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext)
             throws IOException {
-        
-       
-        responseContext.getHeaders().add("Access-Control-Allow-Origin", "*"); 
-        
-        responseContext.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
+
+        // Headers CORS essenciais
+        responseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
+        responseContext.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization, x-requested-with");
         responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
         responseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+        responseContext.getHeaders().add("Access-Control-Max-Age", "3600");
+
+        // Adicionar headers para preflight requests
+        if ("OPTIONS".equals(requestContext.getMethod())) {
+            responseContext.setStatus(200);
+        }
     }
 }

d30e3cb59e71a4d6a87983165ed9b44da2363841
deryksouza2006 <deryksouza2006@gmail.com>
2025-11-07 21:52:23 -0300

    Configuração CORS definitiva para Quarkus

---
+++ src/main/java/com/visuall/config/CorsFilter.java
@@ -0,0 +1,27 @@
+package com.visuall.config;
+
+import io.vertx.core.http.HttpServerRequest;
+import jakarta.ws.rs.container.ContainerRequestContext;
+import jakarta.ws.rs.container.ContainerResponseContext;
+import jakarta.ws.rs.container.ContainerResponseFilter;
+import jakarta.ws.rs.core.Context;
+import jakarta.ws.rs.ext.Provider;
+import java.io.IOException;
+
+@Provider
+public class CorsFilter implements ContainerResponseFilter {
+
+    @Context
+    HttpServerRequest request;
+
+    @Override
+    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
+        responseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
+        responseContext.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization, x-requested-with");
+        responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
+        responseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+        responseContext.getHeaders().add("Access-Control-Max-Age", "86400");
+
+        System.out.println("🎯 CORS Filter aplicado para: " + requestContext.getMethod());
+    }
+}
